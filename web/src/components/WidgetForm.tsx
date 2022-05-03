a38ff6d9c83510ab4bab2d450f521b51d6087abf
Marcos Siega <marcosmsiega@gmail.com>
2022-05-03 18:02:05 -0300

    chore: partial implementation of lesson 2

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,7 +1,13 @@
 export function WidgetForm(){
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">  
-            Hello Marcos!
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+            </header>
+            <p>Hello Marcos!</p>
+            <footer className="text-xs text-neutral-700">
+                Feito com ❤️ pela Rocketseat
+            </footer>
         </div>
     );
 }
