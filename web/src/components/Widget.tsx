be55098a847a9ee5e508eb6cda2116df107eaabb
Marcos Siega <marcosmsiega@gmail.com>
2022-05-03 17:57:51 -0300

    feat: make use of WidgetForm on Feedback Widget component

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,5 +1,6 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm'
 
 interface WidgetParams{
     title:string
@@ -7,8 +8,10 @@ interface WidgetParams{
 
 export function Widget(params:WidgetParams) {
     return (
-        <Popover className='absolute bottom-5 right-5'>
-            <Popover.Panel>Hello Marcos</Popover.Panel>
+        <Popover className='absolute bottom-4 right-4 md:bottom-10 md:right-8 flex flex-col items-end'>
+            <Popover.Panel>
+                <WidgetForm />
+            </Popover.Panel>
             <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6' />
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
