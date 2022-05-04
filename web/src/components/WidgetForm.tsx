5beb5266e972c1deba0116e73417089a58e272e9
Marcos Siega <marcosmsiega@gmail.com>
2022-05-04 11:01:22 -0300

    feat: CloseButton added

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,12 +1,15 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm(){
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">  
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
+                <CloseButton />
             </header>
             <p>Hello Marcos!</p>
-            <footer className="text-xs text-neutral-700">
-                Feito com ❤️ pela Rocketseat
+            <footer className="text-xs text-neutral-400">
+                Feito com ❤️ por <a className="underline underline-offset-1" href="https://github.com/MarcosSiega">Marcos Siega</a> 
             </footer>
         </div>
     );
